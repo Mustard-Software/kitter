0b1a42c0dded62dfed2dfe728f4bdd878125ad12
Drew Lowe <drew@drew.so>
2025-07-21 20:37:19 -0400

    fix postinstall script

--- src/scripts/postinstall.ts
+++ src/scripts/postinstall.ts
@@ -9,29 +9,33 @@ const defaultConfigString = `import type { KitterConfig } from 'kitter';
 export const kitter: KitterConfig = {
   styles: {
     documentation: {
-      maxWidth: '1000px',
-      className: '',
-    },
-    scrollToTop: {
-      textClassName: 'font-mono',
-      iconClassName: '',
-      iconColor: 'black',
-      textOverride: 'table of contents',
-    },
-    page: {
-      className: '',
-    },
-    pageTitle: {
-      className: '',
-    },
-    pageDescription: {
-      className: '',
-    },
-    pageSection: {
-      titleClassName: '',
-    },
-    documentContents: {
-      className: '',
+      documentation: {
+        maxWidth: '250px',
+        className: '',
+      },
+      scrollToTop: {
+        textClassName: 'font-mono',
+        iconClassName: '',
+        iconColor: 'black',
+        textOverride: 'table of contents',
+      },
+      page: {
+        className: '',
+      },
+      pageTitle: {
+        className: 'text-4xl font-light mb-12 pt-20 font-mono',
+      },
+      pageDescription: {
+        className:
+          'text-left text-md font-light mb-20 flex flex-col gap-8 font-mono',
+      },
+      pageSection: {
+        titleClassName: 'text-2xl font-light font-mono text-left w-full',
+      },
+      documentContents: {
+        className:
+          'font-mono underline decoration-dotted grid grid-cols-4 gap-8 mt-8',
+      },
     },
   },
 };
